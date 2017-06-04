7f6973f966815b79b30c87570f3cac0152003ae0
Akhil Dave <akhiltheway@gmail.com>
2017-06-04 19:11:56 +0530

    Rubocop updates

--- bot.rb
+++ bot.rb
@@ -9,30 +9,32 @@ class Bot
   # Find and return's the postion of Princess(p) or Bot(m)
   def find_position(grid, p_m)
     grid.each_with_index do |row, index|
-      @x = index
-      @y = row.index(p_m)
-      break if @y
+      @position_x = index
+      @position_y = row.index(p_m)
+      break if @position_y
     end
-    fail "#{p_m == 'p' ? 'Princess' : 'Bot'} not found in grid." unless @y
-    [@x, @y]
+    fail "#{p_m == 'p' ? 'Princess' : 'Bot'} not found" unless @position_y
+    [@position_x, @position_y]
   end
 
   # Bot rescuing princess
-  def bot_rescuing_princess(bot_x, bot_y, princess_x, princess_y)
-    drows = princess_x - bot_x
-    dcols = princess_y - bot_y
+  def bot_rescuing_princess(bot_pos_x, bot_pos_y, princess_pos_x, princess_pos_y)
+    drows = princess_pos_x - bot_pos_x
+    dcols = princess_pos_y - bot_pos_y
     moves = []
     moves << (drows < 0 ? "UP\n" * drows.abs : "DOWN\n" * drows)
     moves << (dcols < 0 ? "LEFT\n" * dcols.abs : "RIGHT\n" * dcols)
     moves.join('')
   end
 
-  def next_move(_n, x, y, princess, _grid)
-    move = 'UP' if (princess[0] - x) < 0
-    move = 'DOWN' if (princess[0] - x) > 0
-    move = 'LEFT' if (princess[1] - y) < 0
-    move = 'RIGHT' if (princess[1] - y) > 0
+  # Find next move to rescue princess
+  def next_move(_n, bot_pos_x, bot_pos_y, princess_position, _grid)
+    case
+    when princess_position[0] - bot_pos_x < 0 then move = 'UP'
+    when princess_position[0] - bot_pos_x > 0 then move = 'DOWN'
+    when princess_position[1] - bot_pos_y < 0 then move = 'LEFT'
+    when princess_position[1] - bot_pos_y > 0 then move = 'RIGHT'
+    end
     move
   end
-
 end
